9993aa269d94c0368f6f3ec10a343d04054a9d70
Jesse  Tochukwu <uzomaesse@gmail.com>
2023-02-14 11:22:44 +0100

    feat:addede formcontainer back

--- app/navigation/walletNavigation.tsx
+++ app/navigation/walletNavigation.tsx
@@ -1,56 +1,53 @@
-// import React from "react";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import {
-//   VerifyUser,
-//   Registration,
-//   Login,
-//   ForgotPassword,
-//   ChangePassword,
-//   VerifyCode,
-//   OnAccountSuccess,
-//   OnSuccessScreen,
-//   OnPasswordSuccess,
-//   PaymentDetails,
-//   FagList,
-//   Notification,
-//   Election,
-//   ElectionDetails,
-//   ProfileDetails,
-//   Votingstat
-// } from "../screens";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  VerifyUser,
+  Registration,
+  Login,
+  ForgotPassword,
+  ChangePassword,
+  VerifyCode,
+  OnAccountSuccess,
+  OnSuccessScreen,
+  OnPasswordSuccess,
+  PaymentDetails,
+  FagList,
+  Notification,
+  Election,
+  ElectionDetails,
+  ProfileDetails,
+  Votingstat
+} from "../screens";
 
-// const navigationScreenOptions = { headerShown: false };
+const navigationScreenOptions = { headerShown: false };
 
-// const WalletNavigation = () => {
-//   const WalletStack = createNativeStackNavigator();
-//   return (
-//     <WalletStack.Navigator screenOptions={navigationScreenOptions}>
-//       {/* <WalletStack.Screen name="Election" component={Election} />
-//       <WalletStack.Screen name='ProfileDetails' component={ProfileDetails} />
-//       <WalletStack.Screen name='ElectionDetails' component={ElectionDetails} />
-//       <WalletStack.Screen name='Votingstat' component={Votingstat} /> */}
+const WalletNavigation = () => {
+  const WalletStack = createNativeStackNavigator();
+  return (
+    <WalletStack.Navigator screenOptions={navigationScreenOptions}>
+      {/* <WalletStack.Screen name="Election" component={Election} />
+      <WalletStack.Screen name='ProfileDetails' component={ProfileDetails} />
+      <WalletStack.Screen name='ElectionDetails' component={ElectionDetails} />
+      <WalletStack.Screen name='Votingstat' component={Votingstat} /> */}
+      {/* <WalletStack.Screen name="PaymentDetails" component={PaymentDetails} /> */}
+      {/* <WalletStack.Screen name="Notification" component={Notification} /> */}
+      {/* <WalletStack.Screen name="FagList" component={FagList} /> */}
 
-//       {/* <WalletStack.Screen name="PaymentDetails" component={PaymentDetails} /> */}
-//       {/* <WalletStack.Screen name="Notification" component={Notification} /> */}
-//       {/* <WalletStack.Screen name="FagList" component={FagList} /> */}
-//       {/*
-//       <WalletStack.Screen name="Registration" component={Registration} />
-//       <WalletStack.Screen name="Login" component={Login} />
-//       <WalletStack.Screen name="ForgotPassword" component={ForgotPassword} />
-//       <WalletStack.Screen name="ChangePassword" component={ChangePassword} />
-//       <WalletStack.Screen name="VerifyUser" component={VerifyUser} />
-//       <WalletStack.Screen name="VerifyCode" component={VerifyCode} />
-//       <WalletStack.Screen
-//         name="OnAccountSuccess"
-//         component={OnAccountSuccess}
-//       />
-//       <WalletStack.Screen name="onSucess" component={OnSuccessScreen} />
-//       <WalletStack.Screen
-//         name="PasswordChanged"
-//         component={OnPasswordSuccess}
-//       /> */}
-//     </WalletStack.Navigator>
-//   );
-// };
+    
+      <WalletStack.Screen name="Registration" component={Registration} />
+      <WalletStack.Screen name="Login" component={Login} />
+      <WalletStack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <WalletStack.Screen name="ChangePassword" component={ChangePassword} />
+      <WalletStack.Screen name="VerifyUser" component={VerifyUser} />
+      <WalletStack.Screen name="VerifyCode" component={VerifyCode} />
+      <WalletStack.Screen name="OnAccountSuccess" component={OnAccountSuccess}/>
+      <WalletStack.Screen name="onSucess" component={OnSuccessScreen} />
+      <WalletStack.Screen
+        name="PasswordChanged"
+        component={OnPasswordSuccess}
+      /> 
+    </WalletStack.Navigator>
+  );
+};
 
-// export default WalletNavigation;
+export default WalletNavigation;

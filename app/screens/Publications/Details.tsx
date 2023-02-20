f14c4427aba846c79c9cac9da4b7be6b3fcd1c46
japheth-dew <solaadewumi2002@gmail.com>
2023-02-20 19:29:50 +0100

    publication working

--- app/screens/Publications/Details.tsx
+++ app/screens/Publications/Details.tsx
@@ -1,11 +1,17 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { HomeHeader } from "../../components";
-import { useNavigation, useRoute } from "@react-navigation/native";
+import { useRoute } from "@react-navigation/native";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import { FontAwesome } from "@expo/vector-icons";
 import { COLORS } from "../../constants/color";
-import { images } from "../../assets/dummyData";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import LoadingIndicator from "../../utils/LoadingIndicator";
+import {
+  createComment,
+  deleteComment,
+  getComments,
+} from "../../store/slices/news_publication/publicationSlice";
 
 interface DetailsProps {
   route?: any;
@@ -25,22 +31,18 @@ const Like = () => {
   );
 };
 
-const CommentIcon = () => {
-  return (
-    <TouchableOpacity activeOpacity={0.7}>
-      <FontAwesome name="comment" size={24} color={"gray"} />
-    </TouchableOpacity>
-  );
-};
-
 const CommentCard = ({
+  id,
   img,
   name,
   comment,
+  onPress,
 }: {
+  id: string;
   img: any;
   name: string;
   comment: string;
+  onPress: any;
 }) => {
   return (
     <View
@@ -59,17 +61,29 @@ const CommentCard = ({
           marginRight: 10,
         }}
       >
-        <Image source={img} style={{ width: 40, height: 40 }} />
+        <Image source={{ uri: img }} style={{ width: 40, height: 40 }} />
       </View>
       <View
         style={{
           flex: 1,
         }}
       >
-        <Text style={{ fontWeight: "700" }}>{name}</Text>
         <View
           style={{
-            backgroundColor: COLORS.icon,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontWeight: "700" }}>{name}</Text>
+          <TouchableOpacity onPress={() => onPress(id)} activeOpacity={0.7}>
+            <Text style={{ color: "crimson" }}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            borderColor: COLORS.icon,
+            borderWidth: 1,
             padding: 10,
             borderRadius: 10,
           }}
@@ -81,66 +95,50 @@ const CommentCard = ({
   );
 };
 
-const Comment = () => {
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        marginVertical: 10,
-      }}
-    >
-      <View
-        style={{
-          borderRadius: 50,
-          overflow: "hidden",
-          width: 40,
-          height: 40,
-          marginRight: 10,
-        }}
-      >
-        <Image source={images.man} style={{ width: 40, height: 40 }} />
-      </View>
-      <View
-        style={{
-          flex: 1,
-        }}
-      >
-        <Text style={{ fontWeight: "700" }}>John Doe</Text>
-        <View
-          style={{
-            backgroundColor: COLORS.icon,
-            paddingHorizontal: 10,
-            borderRadius: 10,
-          }}
-        >
-          <TextInput
-            numberOfLines={4}
-            style={{ minHeight: 40 }}
-            multiline
-            placeholder="Write a comment"
-          />
-        </View>
-      </View>
-    </View>
-  );
-};
-
 const Details = ({ route, navigation }: DetailsProps) => {
+  const [showAll, setShowAll] = React.useState(3);
   const altRoute = useRoute();
   const data = route?.params?.publication || altRoute?.params || {};
+  const dispatch = useAppDispatch();
 
-  const [showAll, setShowAll] = React.useState(3);
+  const [userComment, setUserComment] = React.useState("");
+
+  const { comments, commenstLoading, publications } = useAppSelector(
+    (state) => state.newsPublication.publication
+  );
 
   const handlePress = () => {
-    setShowAll(showAll === 3 ? data.comments.length : 3);
+    setShowAll(showAll === 3 ? comments?.data?.length : 3);
   };
+
+  const inputRef = React.useRef<any>(null);
+
+  const handleComment = () => {
+    inputRef.current.focus();
+  };
+
+  const handleDelete = (id: string) => {
+    dispatch(deleteComment(id));
+    dispatch(getComments(data.id));
+  };
+
+  const handleSend = () => {
+    if (userComment.length === 0) return;
+    dispatch(createComment(data.id, userComment));
+    setUserComment("");
+    dispatch(getComments(data.id));
+  };
+
+  useEffect(() => {
+    dispatch(getComments(data.id));
+    console.log(data.id);
+  }, [navigation, data.id, dispatch]);
 
   return (
     <>
       <HomeHeader
         navigation={navigation}
-        title={data?.name || "Details " + data.id}
+        title={data.name || "Details " + data.id}
         back={() => navigation.goBack("Publications")}
       />
       <ScrollView
@@ -185,48 +183,11 @@ const Details = ({ route, navigation }: DetailsProps) => {
               {data.name}
             </Text>
             <View>
-              <Text
-                style={{
-                  fontSize: 15,
-                  color: "gray",
-                  textAlign: "justify",
-                }}
-              >
-                {data.body}
-              </Text>
-            </View>
-
-            <View
-              style={{
-                marginVertical: 10,
-              }}
-            >
-              {data.paragraphs.map((item: any) => (
-                <View
-                  style={{
-                    marginVertical: 10,
-                  }}
-                  key={item.id}
-                >
-                  <Text
-                    style={{
-                      fontSize: 20,
-                      color: COLORS.primary,
-                      textAlign: "justify",
-                      fontWeight: "700",
-                    }}
-                  >
-                    {item.heading}
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 15,
-                      color: "gray",
-                      textAlign: "justify",
-                    }}
-                  >
-                    {item.paragragh}
-                  </Text>
+              <Text style={styles.textStyle}>{data.body}</Text>
+              {data?.paragraphs?.map((item: any, index: number) => (
+                <View key={index}>
+                  <Text style={styles.paragraphHeading}>{item.heading}</Text>
+                  <Text style={styles.textStyle}>{item.paragragh}</Text>
                 </View>
               ))}
             </View>
@@ -234,12 +195,12 @@ const Details = ({ route, navigation }: DetailsProps) => {
             <Text
               style={{
                 marginTop: 5,
-                fontSize: 15,
+                fontSize: 13,
                 color: "gray",
                 fontWeight: "700",
               }}
             >
-              {data?.likes === null ? 0 : data.likes} likes
+              {data?.likes ? data.likes : 0} Likes
             </Text>
 
             <View
@@ -255,50 +216,128 @@ const Details = ({ route, navigation }: DetailsProps) => {
               }}
             >
               <Like />
-              <CommentIcon />
+              {/* comment  */}
+              <TouchableOpacity onPress={handleComment} activeOpacity={0.7}>
+                <FontAwesome name="comment" size={24} color={"gray"} />
+              </TouchableOpacity>
             </View>
 
-            {/* Comment section */}
-            <View
-              style={{
-                marginTop: 20,
-              }}
-            >
-              {data?.comments?.slice(0, showAll)?.map((item: any) => (
-                <CommentCard
-                  key={item.id}
-                  img={item.img}
-                  name={item.name}
-                  comment={item.comment}
-                />
-              ))}
-            </View>
+            {commenstLoading ? (
+              <LoadingIndicator />
+            ) : (
+              <>
+                <View
+                  style={{
+                    marginTop: 20,
+                  }}
+                >
+                  {comments?.data?.slice(0, showAll).map((item: any) => (
+                    <CommentCard
+                      onPress={handleDelete}
+                      key={item.id}
+                      id={item.id}
+                      img={item?.member?.photo_url}
+                      name={item?.member?.full_name}
+                      comment={item?.comment}
+                    />
+                  ))}
+                </View>
 
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={handlePress}
-              style={{
-                marginTop: 10,
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Text style={{ color: COLORS.primary, fontWeight: "700" }}>
-                {showAll === 3
-                  ? `View ${data.comments?.length - showAll} more comments`
-                  : "Hide comments"}
-              </Text>
-            </TouchableOpacity>
+                {comments?.data?.length > 3 && (
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    onPress={handlePress}
+                    style={{
+                      marginTop: 10,
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Text style={{ color: COLORS.primary, fontWeight: "700" }}>
+                      {showAll === 3
+                        ? `View ${
+                            comments?.data?.length - showAll
+                          } more comments`
+                        : "Hide comments"}
+                    </Text>
+                  </TouchableOpacity>
+                )}
+              </>
+            )}
           </View>
         </View>
       </ScrollView>
 
-      <Comment />
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginVertical: 10,
+        }}
+      >
+        {/* <View
+        style={{
+          borderRadius: 50,
+          overflow: "hidden",
+          width: 40,
+          height: 40,
+          marginRight: 10,
+        }}
+      >
+        <Image source={images.man} style={{ width: 40, height: 40 }} />
+      </View> */}
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          {/* <Text style={{ fontWeight: "700" }}>John Doe</Text> */}
+          <View
+            style={{
+              backgroundColor: COLORS.icon,
+              paddingHorizontal: 10,
+              borderRadius: 10,
+            }}
+          >
+            <TextInput
+              value={userComment}
+              onChangeText={(text) => setUserComment(text)}
+              ref={inputRef}
+              numberOfLines={4}
+              // style={{ minHeight: 40 }}
+              multiline
+              placeholder="Write a comment"
+            />
+          </View>
+        </View>
+        <TouchableOpacity
+          onPress={handleSend}
+          style={{
+            marginLeft: 10,
+          }}
+          activeOpacity={0.7}
+        >
+          <FontAwesome name="send" size={24} color={"gray"} />
+        </TouchableOpacity>
+      </View>
     </>
   );
 };
 
 export default Details;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  paragraphHeading: {
+    fontSize: 18,
+    fontWeight: "700",
+    marginTop: 20,
+  },
+
+  textStyle: {
+    fontSize: 16,
+    color: "gray",
+    marginVertical: 5,
+    textAlign: "justify",
+  },
+});
--- app/store/slices/news_publication/publicationSlice.ts
+++ app/store/slices/news_publication/publicationSlice.ts
@@ -6,10 +6,14 @@ import { PRE_URL } from "../../../utils/ENV/envs";
 
 const initialState: {
   publications: any;
+  comments: any;
   loading: boolean;
+  commenstLoading: boolean;
 } = {
   publications: [],
+  comments: [],
   loading: false,
+  commenstLoading: false,
 };
 
 const publicationSlice = createSlice({
@@ -27,11 +31,30 @@ const publicationSlice = createSlice({
     publicationRequestFailed: (state, action) => {
       state.loading = false;
     },
+    commentRequested: (state, action) => {
+      state.commenstLoading = true;
+    },
+
+    commentReceived: (state, action) => {
+      state.commenstLoading = false;
+      state.comments = action.payload;
+      console.log("action.payload", action.payload);
+    },
+
+    commentRequestFailed: (state, action) => {
+      state.commenstLoading = false;
+    },
   },
 });
 
-const { publicationRequested, publicationReceived, publicationRequestFailed } =
-  publicationSlice.actions;
+const {
+  publicationRequested,
+  publicationReceived,
+  publicationRequestFailed,
+  commentReceived,
+  commentRequestFailed,
+  commentRequested,
+} = publicationSlice.actions;
 
 export default publicationSlice.reducer;
 
@@ -67,5 +90,96 @@ export const getPublication =
     } catch (error: any) {
       console.error("An error occured getting publication", error);
       dispatch(publicationRequestFailed(error.message));
+    }
+  };
+
+export const getComments =
+  (publicationId: string) => async (dispatch: AppDispatch) => {
+    try {
+      const getToken: any = await retrieveUserDetails();
+
+      if (getToken && getToken.token) {
+        const token = getToken.token;
+
+        dispatch(
+          apiCallBegan({
+            url:
+              PRE_URL +
+              `publication/publicationview__comment/?publication_id=${publicationId}`,
+            extraheaders: "Token " + token,
+            method: "get",
+            onStart: commentRequested.type,
+            onSuccess: commentReceived.type,
+            onError: commentRequestFailed.type,
+          })
+        );
+      } else {
+        const error = new Error("Unable to retrieve user token");
+        console.error(error);
+        dispatch(commentRequestFailed(error.message));
+      }
+    } catch (error: any) {
+      console.error("An error occured getting comments", error);
+      dispatch(commentRequestFailed(error.message));
+    }
+  };
+
+export const createComment =
+  (publicationId: string, comment: string) => async (dispatch: AppDispatch) => {
+    try {
+      const getToken: any = await retrieveUserDetails();
+
+      if (getToken && getToken.token) {
+        const token = getToken.token;
+        dispatch(
+          apiCallBegan({
+            url: PRE_URL + `publication/publicationview__comment/`,
+            extraheaders: "Token " + token,
+            method: "post",
+            data: {
+              news: publicationId,
+              comment: comment,
+            },
+            onStart: commentRequested.type,
+            onSuccess: commentReceived.type,
+            onError: commentRequestFailed.type,
+          })
+        );
+      } else {
+        const error = new Error("Unable to retrieve user token");
+        console.error(error);
+        dispatch(commentRequestFailed(error.message));
+      }
+    } catch (error: any) {
+      console.error("An error occured getting comments", error);
+      dispatch(commentRequestFailed(error.message));
+    }
+  };
+
+export const deleteComment =
+  (commentId: string) => async (dispatch: AppDispatch) => {
+    try {
+      const getToken: any = await retrieveUserDetails();
+
+      if (getToken && getToken.token) {
+        const token = getToken.token;
+        dispatch(
+          apiCallBegan({
+            url: PRE_URL + `publication/publicationview__comment/${commentId}/`,
+            extraheaders: "Token " + token,
+            method: "delete",
+            onStart: commentRequested.type,
+            onSuccess: commentReceived.type,
+            onError: commentRequestFailed.type,
+          })
+        );
+      } else {
+        const error = new Error("Unable to retrieve user token");
+        console.error(error);
+        dispatch(commentRequestFailed(error.message));
+      }
+    } catch (error: any) {
+      console.error("An error occured getting comments", error);
+      dispatch(commentRequestFailed(error.message));
     }
   };
